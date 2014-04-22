9385ca5949e924172613839b7f02c12c6c68fcfc
narkisr <narkisr@gmail.com>
2014-04-22 19:03:26 +0300

    Adding license

--- project.clj
+++ project.clj
@@ -1,6 +1,5 @@
 (defproject narkisr/lein-template "0.0.1"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :license  {:name "Apache License, Version 2.0" :url "http://www.apache.org/licenses/LICENSE-2.0.html"}
   :eval-in-leiningen true)
--- src/leiningen/new/narkisr/core.clj
+++ src/leiningen/new/narkisr/core.clj
@@ -1,3 +1,2 @@
-
 (ns {{name}}.core)
 
